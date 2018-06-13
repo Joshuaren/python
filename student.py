177a08a3e91f5d9ff7338348b4aa42d248414ad9
Joshua <joshuaren@163.com>
2018-06-13 22:11:40 +0800

    类

---
+++ student.py
@@ -0,0 +1,7 @@
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+
+    def get_gender(self):
+        return self.__genderbartb
