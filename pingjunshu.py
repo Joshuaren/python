6a89c510a922ee7543044decd7361be00be03af2
Joshua <joshuaren@163.com>
2018-06-19 23:18:52 +0800

    QiuPingJunShu

---
+++ pingjunshu.py
@@ -0,0 +1,11 @@
+N = 10
+sum = 0
+count = 0
+print('input 10 numbers: ')
+while count < N:
+    number = float(input())
+    sum += number
+    count += 1
+average = sum / N
+print('N = {}, Sum = {}'.format(N,sum))
+print('Average = {:.2f}'.format(average))
